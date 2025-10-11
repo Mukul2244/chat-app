e847e8401ddc45eac3007215237000458d45dbf6
Mukul2244 <mukulsharma1594@gmail.com>
2025-10-11 22:50:35 +0530

    feat: update webhook handling and add Radix UI tooltip dependency

--- convex/http.ts
+++ convex/http.ts
@@ -1,8 +1,10 @@
 import { httpRouter } from "convex/server";
 import { httpAction } from "./_generated/server";
-import { Webhook, WebhookEvent } from "svix";
+import { Webhook } from "svix";
+import type { WebhookEvent } from "@clerk/nextjs/server";
+import { internal } from "./_generated/api";
 
-const validatePayload = async(req: Request): Promise<WebhookEvent | undefined>  => {
+const validatePayload = async (req: Request): Promise<WebhookEvent | undefined> => {
     const payload = await req.text();
 
     const svixHeaders = {
@@ -18,22 +20,42 @@ const validatePayload = async(req: Request): Promise<WebhookEvent | undefined>  
     } catch (error) {
         console.error("Clerk webhook request could not be verified")
     }
-    }
+}
 
-
-const handleClerkWebhook = httpAction(async (ctx,req)=>{
+const handleClerkWebhook = httpAction(async (ctx, req) => {
     const event = await validatePayload(req);
 
-    if(!event){
-        return new Response("Could not validate clerk payload", {status: 400});
+    if (!event) {
+        return new Response("Could not validate clerk payload", { status: 400 });
     }
-    
+    switch (event.type){
+        case "user.created":
+            const user = await ctx.runQuery(internal.user.get,{clerkId: event.data.id});
+            if(user){
+                console.log(`Updating user ${event.data.id} with : ${event.data}`)
+            }
+        case "user.updated": {
+             console.log(`Creating/Updating user : ${event.data.id}`);
+
+            await ctx.runMutation(internal.user.create,{
+                username :`${event.data.first_name} ${event.data.last_name}`,
+                imageUrl: event.data.image_url,
+                clerkId: event.data.id,
+                email: event.data.email_addresses[0].email_address,
+            })
+            break;
+        }
+        default : {
+            console.log(`Clerk webhook event not supported : ${event.type}`);
+        }     
+    }
+    return new Response(null, { status: 200 });
 })
 
 const http = httpRouter();
 
 http.route({
-    path : "/clerk-users-webhook",
+    path: "/clerk-users-webhook",
     method: "POST",
     handler: handleClerkWebhook
 })
